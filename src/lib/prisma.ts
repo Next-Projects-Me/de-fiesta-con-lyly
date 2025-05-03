81185cbf2a2260c142e9881ea7f55d88f175269d
Alejandrovaz9522 <rodrigo.vasquez@ithealth.co>
2025-05-03 15:35:44 -0500

    Update prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,9 @@
 
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const prismaClientSingleton = () => {
-    return new PrismaClient();
+    return new PrismaClient().$extends(withAccelerate());
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
@@ -15,4 +16,4 @@ const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
